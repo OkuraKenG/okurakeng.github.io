37f8eb8ad07b0bc08abe05b532699fc229c4ef7d
Kenji Okura <128434434+okurakeng@users.noreply.github.com>
2024-08-30 23:25:26 -0400

    moon

--- sketch.js
+++ sketch.js
@@ -1,145 +1,47 @@
-let canvas;
-let sunDiameter;
-
-let solarSystemCenterX;
-let solarSystemCenterY;
-
-let planetList;
-let starList = [];
-
-let x;
-let y;
-let deg = 0;
-
-let cometList = [];
+let solarSystem;
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0,0);
-  canvas.style('z-index','-1');
-  assignSolarSysCords();
-  drawBaseSolarSys();
+  let canvas = createCanvas(windowWidth,  document.documentElement.scrollHeight);
+  canvas.position(0, 0);
+  canvas.style('z-index', '-1');
+  solarSystem = initiateSolarSystem();
+  solarSystem.drawBase();
+  pixelDensity(2)
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, document.documentElement.scrollHeight);
+  solarSystem = initiateSolarSystem();
+  solarSystem.drawBase();
 }
 
 function draw() {
-  drawBaseSolarSys();
-
-  for (const obj of planetList) {
-    noStroke();
-
-    fill(obj.colour);
-    x = solarSystemCenterX + (obj.diameter / 2) * cos(obj.deg);
-    y = solarSystemCenterY + (obj.diameter / 2) * sin(obj.deg);
-    circle(x, y, obj.size);
-    obj.deg += 0.0095;
-  }
-  
+  solarSystem.drawBase();
+  solarSystem.draw();
 }
 
-const assignSolarSysCords = () => {
-  sunDiameter = random(25, 50);
+const initiateSolarSystem = () => {
+  const sunDiameter = random(40, 50);
 
-  solarSystemCenterX = random(windowWidth/2, windowWidth - sunDiameter * 2);
-  solarSystemCenterY = random(windowHeight/2, windowHeight - sunDiameter * 2);
 
-  mercury = new Planet(sunDiameter * random(2, 3));
-  venus = new Planet(sunDiameter * random(4, 5));
-  earth = new Planet(sunDiameter * random(8, 11));
-  mars = new Planet(sunDiameter * random(14, 15));
-  jupiter = new LargePlanet(sunDiameter * random(22, 24));
-  saturn = new LargePlanet(sunDiameter * random(26, 27));
-  uranus = new LargePlanet(sunDiameter * random(30, 31));
-  neptune = new LargePlanet(sunDiameter * random(35, 36));
-  planetList = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune];
+  const solarSystemCenterX = random(windowWidth / 2, windowWidth - sunDiameter * 2);
+  const solarSystemCenterY = random(windowHeight / 2, windowHeight - sunDiameter * 2);
+
+  const earth = new Planet(sunDiameter * random(8, 11), random(0,360), random(10,15));
+  const earth_moon = new System(1,solarSystemCenterX + (earth.semiMajorAxis / 2) * cos(earth.deg) , solarSystemCenterY + (earth.semiMajorAxis / 2) * sin(earth.deg), 0.05, [new Planet(30,0,7.5)], earth)
+
+  const rate = 0.008;
+  const bodies = [
+    new Planet(sunDiameter * random(3, 4), random(0,360), random(10,15)), // m
+    new Planet(sunDiameter * random(5, 6), random(0,360), random(10,15)), // v
+    new Planet(sunDiameter * random(14, 15), random(0,360), random(10,15)), // m
+    new Planet(sunDiameter * random(22, 24), random(0,360), random(25,35)), 
+    new Planet(sunDiameter * random(26, 27), random(0,360), random(25,35)),
+    new Planet(sunDiameter * random(30, 31), random(0,360), random(25,35)),
+    new Planet(sunDiameter * random(35, 36), random(0,360), random(25,35)),
+    earth, // e
+    earth_moon];
   
-  for (let i = 0 ; i < 200 ; i++ ) {
-    starList.push(new Star((Math.random()-0.5)/40));
-  }
-
-  for (let i = 0 ; i < 2 ; i++ ) {
-    cometList.push(new Comet());
-  }
-
-  console.log(cometList)
-
+  return new System(sunDiameter,solarSystemCenterX, solarSystemCenterY, rate, bodies);
 };
 
-function drawBaseSolarSys() {
-  background(25);
-
-  strokeWeight(2);
-  drawStars();
-  drawSun();
-
-
-  for (const comet of cometList) {
-    comet.draw()
-  }
-
-
-  stroke(75);
-  strokeWeight(1);
-  noFill();
-
-  for (const obj of planetList) {
-    circle(solarSystemCenterX, solarSystemCenterY, obj.diameter);
-  }
-}
-
-function drawStars() {
-  for (const star of starList) {
-    stroke( star.bright)
-    point( star.x, star.y);
-    star.x += star.bias;
-    star.y += star.bias;
-  }
-}
-
-function drawSun() {
-  noStroke();
-  fill(255, 204, 0);
-  circle(solarSystemCenterX, solarSystemCenterY, 50);
-}
-
-
-const Planet = class {
-  constructor(diameter) {
-    this.diameter = diameter;
-    this.deg = random(0, 360);
-    this.size = random(10, 20);
-    this.colour = color(random(255), 200, random(255));
-  }
-};
-
-class LargePlanet extends Planet {
-  constructor(diameter) {
-    super(diameter);
-    this.size = random(20, 30);
-  }
-}
-
-class Star {
-   constructor(bias) {
-    this.x = random(0, windowWidth);
-    this.y = random(0, windowHeight);
-    this.bright = random(50, 150);
-    this.bias = bias;
-  } 
-}
-
-class Comet {
-  constructor() {
-    this.swingyness = random(150, 250);
-    this.offset = random(32, 100);
-    this.h = random(-75,-25);
-    this.k = ((solarSystemCenterX - this.h)*(solarSystemCenterX - this.h))/(4*this.swingyness) + solarSystemCenterY - this.offset;
-  }  
-
-  draw() {
-    noStroke();
-    fill('red');
-    text('☄️',this.h,this.k)
-    this.h = this.h + 1;
-    this.k = ((solarSystemCenterX - this.h)*(solarSystemCenterX - this.h))/(4*this.swingyness) + solarSystemCenterY - this.offset;
-  }
-}
---
+++ system.js
@@ -0,0 +1,132 @@
+class System {
+    constructor(mainBodyDiameter, x, y, rate, bodies, linkedBody) {
+        this.mainBodyDiameter = mainBodyDiameter;
+        this.x = x;
+        this.y = y;
+        this.rate = rate;
+        this.bodies = bodies;
+        this.stars = [];
+        this.comets = [];
+        this.linkedBody = linkedBody ?? undefined;
+
+        for (let i = 0; i < 200; i++) {
+            this.stars.push(new Star((Math.random() - 0.5) / 40));
+        }
+
+        for (let i = 0; i < 2; i++) {
+            this.comets.push(new Comet(this.x, this.y));
+        }
+    }
+
+    drawBase() {
+        background(25);
+
+        this.#drawStars();
+        this.#drawSun();
+        this.drawOrbits();
+    }
+
+    #drawSun() {
+        noStroke();
+        fill(255, 204, 0);
+        circle(this.x, this.y, this.mainBodyDiameter);
+    }
+
+
+    #drawStars() {
+        strokeWeight(2);
+        for (const star of this.stars) {
+            star.draw();
+        }
+    }
+
+    drawOrbits() {
+        stroke(75)
+        strokeWeight(2);
+        noFill();
+
+        for (const body of this.bodies) { 
+            body.semiMajorAxis ? circle(this.x, this.y, body.semiMajorAxis) : body.drawOrbits();
+        }
+    }
+
+
+    draw() {
+        this.#drawBodies();
+        this.#drawComets();
+
+        if (this.linkedBody) {
+            this.x = this.linkedBody.x;
+            this.y = this.linkedBody.y;
+        }
+    }
+
+    #drawBodies() {
+        for (let body of this.bodies) {
+            body.draw(this.x, this.y, this.rate);
+        }
+
+    }
+
+    #drawComets() {
+        for (let comet of this.comets) {
+            comet.draw(this.x, this.y);
+        }
+    }
+}
+
+class Planet {
+    constructor(semiMajorAxis, startDeg, size, colour) {
+        this.semiMajorAxis = semiMajorAxis;
+        this.deg = startDeg ?? random(0, 360);
+        this.size = size ?? random(10, 20);
+        this.colour = colour ?? color(random(255), 200, random(255));
+        // this.x 
+        // this.y 
+    }
+
+    draw(solarSystemCenterX, solarSystemCenterY, rate) {
+        noStroke();
+        fill(this.colour);
+        this.x = solarSystemCenterX + (this.semiMajorAxis / 2) * cos(this.deg);
+        this.y = solarSystemCenterY + (this.semiMajorAxis / 2) * sin(this.deg);
+
+        circle(this.x, this.y, this.size);
+        this.deg += rate;
+    }
+};
+
+
+class Star {
+    constructor(bias) {
+        this.x = random(0, windowWidth);
+        this.y = random(0, windowHeight);
+        this.bright = random(50, 150);
+        this.bias = bias;
+    }
+
+    draw() {
+        stroke(this.bright)
+        point(this.x, this.y);
+        this.x += this.bias;
+        this.y += this.bias;
+    }
+}
+
+
+class Comet {
+    constructor(solarSystemCenterX, solarSystemCenterY) {
+        this.swingyness = random(150, 250);
+        this.offset = random(32, 100);
+        this.h = random(-75, -25);
+        this.k = ((solarSystemCenterX - this.h) * (solarSystemCenterX - this.h)) / (4 * this.swingyness) + solarSystemCenterY - this.offset;
+    }
+
+    draw(solarSystemCenterX, solarSystemCenterY) {
+        noStroke();
+        fill('red');
+        text('☄️', this.h, this.k)
+        this.h = this.h + 1;
+        this.k = ((solarSystemCenterX - this.h) * (solarSystemCenterX - this.h)) / (4 * this.swingyness) + solarSystemCenterY - this.offset;
+    }
+}
