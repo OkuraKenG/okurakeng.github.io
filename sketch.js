e3b85e74b1bff9045c83c0002c8efa7b5d3b652f
Kenji Okura <128434434+okurakeng@users.noreply.github.com>
2024-08-31 16:44:31 -0400

    final types of bg

--- sketch.js
+++ sketch.js
@@ -1,8 +1,54 @@
 let solarSystem;
 let nightSky;
+let nightSkyRT;
+let myData;
 let type = 'solar';
+let options = ['weather', 'solar', 'streak', 'rt'];
+
+const links = [
+  "https://api.weather.gov/gridpoints/AER/75,95/forecast/hourly", //pacfic 
+  "https://api.weather.gov/gridpoints/EPZ/87,111/forecast/hourly", // new mexico
+  "https://api.weather.gov/gridpoints/MAF/21,113/forecast/hourly",// blue
+  "https://api.weather.gov/gridpoints/BRO/91,9/forecast/hourly", // starbase
+  "https://api.weather.gov/gridpoints/MLB/58,70/forecast/hourly",// cape
+  "https://api.weather.gov/gridpoints/AKQ/112,102/forecast/hourly", // mid atl
+  "https://api.weather.gov/gridpoints/LOX/74,86/forecast/hourly"  // vandy
+]
+
+
+function preload() {
+  loadJSON(random(links), handleSuccess, handleError);
+}
+
+function handleSuccess(data) {
+  myData = data;
+  myData = myData.properties.periods.map(
+    (forecast) => {
+      return {
+        temperature: forecast.temperature,
+        dewpoint: forecast.dewpoint.value * (9 / 5) + 32,
+        relativeHumidity: forecast.relativeHumidity.value,
+        probabilityOfPrecipitation: forecast.probabilityOfPrecipitation.value,
+        windSpeed: forecast.windSpeed,
+        windDirection: forecast.windDirection
+      }
+
+    }
+  )
+}
+
+// Log any errors to the console.
+function handleError(error) {
+  options.pop();
+}
+
 function setup() {
-  type = random(['solar', 'streak'])
+  for (let i = 0; i < 100; i++) {
+    options.push('solar')
+    options.push('streak')
+    options.push('rt')
+  }
+  type = random(options);
   let canvas = createCanvas(windowWidth, document.documentElement.scrollHeight);
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
@@ -15,8 +61,14 @@ function setup() {
       break;
     case 'streak':
       angleMode(DEGREES);
-      nightSky = new NightSky(random(windowWidth), random(windowHeight));
-      background(15,0,35);
+      nightSky = new NightSky(random(windowWidth / 2, windowWidth), random(windowHeight / 2, windowHeight - 80));
+      break;
+    case 'rt':
+      angleMode(DEGREES);
+      nightSkyRT = new NightSkyRT(random(windowWidth / 2, windowWidth), random(windowHeight / 2, windowHeight - 80));
+      break;
+    case 'weather':
+      drawWeather();
       break;
   }
 }
@@ -30,16 +82,33 @@ function windowResized() {
       solarSystem.drawBase();
       break;
     case 'streak':
-      background(1);
+      angleMode(DEGREES)
+      nightSky = new NightSky(random(0, windowWidth), random(50, windowHeight - 50));
+      break;
+      case 'rt':
+        nightSkyRT = new NightSkyRT(random(windowWidth / 2, windowWidth), random(windowHeight / 2, windowHeight - 80));
+        break;
+    case 'weather':
+      drawWeather();
       break;
   }
 
 }
 
 function mouseClicked(event) {
-  // Code to run that uses the event.
-  if (dist(event.x, event.y, solarSystem.x, solarSystem.y) < (solarSystem.mainBodyDiameter / 2))
-    boom();
+
+  switch (type) {
+    case 'solar':
+      if (dist(event.x, event.y, solarSystem.x, solarSystem.y) < (solarSystem.mainBodyDiameter / 2))
+        boom();
+    case 'streak':
+      if (dist(event.x, event.y, nightSky.x, nightSky.y) < 2.5)
+        boom();
+      break;
+    case 'weather':
+      break;
+  }
+
 }
 
 function draw() {
@@ -51,6 +120,11 @@ function draw() {
       break;
     case 'streak':
       nightSky.draw();
+      break;
+    case 'rt':
+      nightSkyRT.draw();
+    break;
+    case 'weather':
       break;
   }
 
@@ -88,6 +162,81 @@ const initiateSolarSystem = () => {
 
   return new System(sunDiameter, solarSystemCenterX, solarSystemCenterY, rate, bodies);
 };
+
+function drawWeather() {
+  background(22)
+  myData = myData.map(
+    (forecast) => {
+      return {
+        temperature: map(forecast.temperature, 0, 120, windowHeight, 100),
+        dewpoint: map(forecast.dewpoint, 0, 120, windowHeight, 100),
+        relativeHumidity: map(forecast.relativeHumidity, 0, 120, windowHeight, 100),
+        probabilityOfPrecipitation: map(forecast.probabilityOfPrecipitation, 0, 120, windowHeight, 100),
+        windSpeed: map(Number.parseFloat(forecast.windSpeed.substring(0, forecast.windSpeed.indexOf(' '))), 0, 50, windowHeight, 100),
+        windDirection: forecast.windDirection
+      }
+
+    });
+
+  let x = 0;
+  const x_diff = windowWidth / myData.length;
+  noFill();
+
+  stroke('green');
+  beginShape();
+  for (let i = 0; i < myData.length; i++) {
+    vertex(x, myData[i].temperature);
+    x += x_diff;
+  }
+  // Stop drawing the shape.
+  endShape();
+
+  x = 0;
+
+  stroke('blue');
+  beginShape();
+  for (let i = 0; i < myData.length; i++) {
+    vertex(x, myData[i].dewpoint);
+    x += x_diff;
+  }
+  // Stop drawing the shape.
+  endShape();
+
+  x = 0;
+
+  stroke('red');
+  beginShape();
+  for (let i = 0; i < myData.length; i++) {
+    vertex(x, myData[i].relativeHumidity);
+    x += x_diff;
+  }
+  // Stop drawing the shape.
+  endShape();
+
+  x = 0;
+
+  stroke('orange');
+  beginShape();
+  for (let i = 0; i < myData.length; i++) {
+    vertex(x, myData[i].probabilityOfPrecipitation);
+    x += x_diff;
+  }
+  // Stop drawing the shape.
+  endShape();
+
+  x = 0;
+
+  stroke('cyan');
+  beginShape();
+  for (let i = 0; i < myData.length; i++) {
+    vertex(x, myData[i].windSpeed);
+    x += x_diff;
+  }
+  // Stop drawing the shape.
+  endShape();
+
+
+}
 
 
 function boom() {
