239448d2f74069234bf3910780d79d14b3f51d8a
Kenji Okura <okurakeng@gmail.com>
2025-05-28 22:08:59 -0400

    Updating previous content

--- src/components/tabs/AboutMe.tsx
+++ src/components/tabs/AboutMe.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Divider from '@mui/material/Divider';
-import meow from '../../assets/meow.jpg'; // Tell webpack this JS file uses this image
 
 export default function AboutMe() {
 	return (<>
@@ -8,10 +7,10 @@ export default function AboutMe() {
 		<Divider sx={{ bgcolor: 'white' }} className='my-2' />
 		<div className='text-white'>
 			<p>Hello 🙋‍♂️! I&apos;m a recent graduate 🎓 of Pace University with a degree in Computer Science.</p>
-			<p>I currently work as a web-developer for <a href="https://www.phormulary.com/">Phormulary</a> and part time as a <a href="https://www.iona.edu/admissions-financial-aid/high-school-student-programs-iona-university/science-and-technology-entry">STEP Instructor</a> at Iona University.</p>
+			<p>I currently working toward my Master&apos;s degree. I work part time as a <a href="https://www.iona.edu/admissions-financial-aid/high-school-student-programs-iona-university/science-and-technology-entry">STEP Instructor</a> at Iona University.</p>
+			<p>I also intern at <a href="https://bluecolab.pace.edu/">Blue CoLab</a> helping out with app development, kiosk development, data analysis, and backend-apis. Small part of the great things done at Blue CoLab.</p>
 			<p>Please enjoy your stay on okaykenji.github.io. 🙇</p>
 			<p>btw try refreshing the page a few times to see different backgrounds ✨✨✨. Here&apos;s a cat as a bonus:</p>
-			<img src={meow} alt="it goes meow" width="100px" />
 		</div>
 	</>)
 }
--- src/components/tabs/projects/AquaWatchMobile.tsx
+++ src/components/tabs/projects/AquaWatchMobile.tsx
@@ -23,17 +23,13 @@ export default function AquaWatchMobile({ togglePanelVisibility }: AquaWatchMobi
 		<TimelineContent>
 			<div className='flow-root'>
 				<p className='p-1 m-0 float-left font-bold'><i>AquaWatch Mobile</i> (<a href='https://github.com/bluecolab/BlueColab_MobileDataViz'>view</a>)</p>
-				<p className=' p-1 pl-2 m-0 font-bold float-right'>Sept. 2023 - May 2024</p>
+				<p className=' p-1 pl-2 m-0 font-bold float-right'>Sept. 2023 - May 2024; January 2025 - Present</p>
 			</div>
 			<p className=' p-1 pl-2 m-0 text-sm'>💧 Water, you drink it, no? Wanna now whats in it so you don&apos;t die 💀? Well sorry we&apos;re not there yet. But this app services as a wonderful first steps.</p>
 			<p className=' p-1 pl-2 m-0 text-sm font-bold'>Key Technologies:</p>
 			<ul className='list-disc text-sm'>
 				<li>React Native/Expo (app) in JavaScript</li>
-				<li>Shiny (embedded webapp) in Python (R initially)
-					<ul>
-						<li>Data analysis libraries used: plotly, pandas, dataretrieval</li>
-					</ul>
-				</li>
+				<li>Victory Native (embedded visualizations) in JavaScript</li>
 			</ul>
 			<Stack className='mt-2'>
 				<Button onClick={() => {
