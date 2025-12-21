fffa48b926cb64ea229d354c17b3e56ca9a7da9e
Kenji Okura <okurakeng@gmail.com>
2025-12-21 16:46:19 -0500

    Init start

--- src/assets/icons/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Expo } from './expo.png';
-export { default as JS } from './js.png';
-export { default as Plotly } from './plotly.png';
-export { default as Python } from './python.png';
-export { default as R } from './r.png';
-export { default as ReactLogo } from './react.png';
-export { default as Shiny } from './shiny.png';
-export { default as Ionic } from './ionic.png';
-export { default as TS } from './ts.png';
