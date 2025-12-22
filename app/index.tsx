08dc551e5b9913dd29d49510e6314859f68115cf
Kenji Okura <okurakeng@gmail.com>
2025-12-22 11:34:48 -0500

    Update icons

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,12 @@
-import { Stack, Link } from 'expo-router';
-
+import P5Background from '@/components/P5Background';
+import SkiaPlanetExample from '@/components/SkiaPlanetExample';
 import { View } from 'react-native';
 
-import { Button } from '@/components/Button';
-import { Container } from '@/components/Container';
-import { ScreenContent } from '@/components/ScreenContent';
-
 export default function Home() {
-  return (
-    <View className={styles.container}>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
-      </Container>
-    </View>
-  );
+	return (
+		<View>
+			{/* <P5Background /> */}
+			{/* <SkiaPlanetExample /> */}
+		</View>
+	);
 }
-
-const styles = {
-  container: 'flex flex-1 bg-white',
-};
