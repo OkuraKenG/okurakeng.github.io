49e8072b18faad52053ba6339247f895eb97e50e
Kenji Okura <okurakeng@gmail.com>
2026-01-16 00:22:23 -0500

    Attempt at preventing text being cut off

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,11 @@ import { Stack } from 'expo-router';
 
 export default function Layout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        presentation: 'transparentModal',
+      }}>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen
         name="modal"
